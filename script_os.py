cdda04dd9612dd5ee6eb47ce5d515fdee61154c3
Goncharov Anton <a.goncharrov@icloud.com>
2025-01-04 11:58:47 +0300

    add os.mkdir action

--- script_os.py
+++ script_os.py
@@ -7,12 +7,19 @@ https://docs-python.ru/standart-library/modul-pathlib-python/modul-pathlib-vs-mo
 
 # Получение абсолютного пути к текущему исполняемому файлу
 CURRENT_FILE = os.path.abspath(__file__)
-print(CURRENT_FILE)  # Вывод абсолютного пути к текущему файлу
+print(f"Абсолютный путь к текущему файлу: {CURRENT_FILE}")
 
 # Получение директории, в которой находится текущий файл
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)
-print(CURRENT_DIR)  # Вывод директории текущего файла
+print(f"Директория текущего файла: {CURRENT_DIR}")
 
 # Формирование пути к папке 'tmp' внутри директории текущего файла
 TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
-print(TMP_DIR)  # Вывод пути к директории 'tmp'
+print(f"Путь к папке 'tmp': {TMP_DIR}")
+
+# Проверка существования директории 'tmp' и создание, если она отсутствует
+if not os.path.exists(TMP_DIR):
+    os.mkdir(TMP_DIR)  # Создаём папку 'tmp'
+    print(f"Папка 'tmp' создана по пути: {TMP_DIR}")
+else:
+    print(f"Папка 'tmp' уже существует по пути: {TMP_DIR}")
